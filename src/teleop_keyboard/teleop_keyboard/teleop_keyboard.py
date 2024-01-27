637d8f53c458cd084755c4f8bec127d1efd4afac
FemiAyoade <ayoadefemo@gmail.com>
2024-01-27 10:45:40 -0600

    teleop bug fixed

--- src/teleop_keyboard/teleop_keyboard/teleop_keyboard.py
+++ src/teleop_keyboard/teleop_keyboard/teleop_keyboard.py
@@ -6,9 +6,9 @@ from geometry_msgs.msg import Twist
 import sys, select, os
 
 if os.name == 'nt':
-  import msvcrt, time
+    import msvcrt, time
 else:
-  import tty, termios
+    import tty, termios
 
 MAX_LIN_VEL = 0.26
 MAX_ANG_VEL = 1.82
@@ -36,11 +36,11 @@ e = """
 Communications Failed
 """
 
-def getKey():
+def getKey(settings):
     if os.name == 'nt':
         timeout = 0.1
         startTime = time.time()
-        while(1):
+        while True:
             if msvcrt.kbhit():
                 if sys.version_info[0] >= 3:
                     return msvcrt.getch().decode()
@@ -60,13 +60,13 @@ def getKey():
     return key
 
 def vels(target_linear_vel, target_angular_vel):
-    return "currently:\tlinear vel %s\t angular vel %s " % (target_linear_vel,target_angular_vel)
+    return "currently:\tlinear vel %s\tangular vel %s " % (target_linear_vel, target_angular_vel)
 
 def makeSimpleProfile(output, input, slop):
     if input > output:
-        output = min( input, output + slop )
+        output = min(input, output + slop)
     elif input < output:
-        output = max( input, output - slop )
+        output = max(input, output - slop)
     else:
         output = input
 
@@ -74,70 +74,72 @@ def makeSimpleProfile(output, input, slop):
 
 def constrain(input, low, high):
     if input < low:
-      input = low
+        input = low
     elif input > high:
-      input = high
+        input = high
     else:
-      input = input
+        input = input
 
     return input
 
 def checkLinearLimitVelocity(vel):
     vel = constrain(vel, -MAX_LIN_VEL, MAX_LIN_VEL)
-
     return vel
 
 def checkAngularLimitVelocity(vel):
     vel = constrain(vel, -MAX_ANG_VEL, MAX_ANG_VEL)
-
     return vel
 
-if __name__=="__main__":
+def main():
     if os.name != 'nt':
         settings = termios.tcgetattr(sys.stdin)
 
     rclpy.init()
     node = rclpy.create_node("teleop_keyboard")
     qos = QoSProfile(depth=10)
-    pub = node.create_publisher('cmd_vel', Twist, qos)
-    target_linear_vel   = 0.0
-    target_angular_vel  = 0.0
-    control_linear_vel  = 0.0
+    pub = node.create_publisher(Twist, 'cmd_vel', qos)
+    target_linear_vel = 0.0
+    target_angular_vel = 0.0
+    control_linear_vel = 0.0
     control_angular_vel = 0.0
 
     try:
         print(msg)
-        while(1):
-            key = getKey()
-            if key == 'w' :
+        while True:
+            key = getKey(settings)
+            if key == 'w':
                 target_linear_vel = checkLinearLimitVelocity(target_linear_vel + LIN_VEL_STEP_SIZE)
-                print(vels(target_linear_vel,target_angular_vel))
-            elif key == 'x' :
+                print(vels(target_linear_vel, target_angular_vel))
+            elif key == 'x':
                 target_linear_vel = checkLinearLimitVelocity(target_linear_vel - LIN_VEL_STEP_SIZE)
-                print(vels(target_linear_vel,target_angular_vel))
-            elif key == 'a' :
+                print(vels(target_linear_vel, target_angular_vel))
+            elif key == 'a':
                 target_angular_vel = checkAngularLimitVelocity(target_angular_vel + ANG_VEL_STEP_SIZE)
-                print(vels(target_linear_vel,target_angular_vel))
-            elif key == 'd' :
+                print(vels(target_linear_vel, target_angular_vel))
+            elif key == 'd':
                 target_angular_vel = checkAngularLimitVelocity(target_angular_vel - ANG_VEL_STEP_SIZE)
-                print(vels(target_linear_vel,target_angular_vel))
-            elif key == ' ' or key == 's' :
-                target_linear_vel   = 0.0
-                control_linear_vel  = 0.0
-                target_angular_vel  = 0.0
+                print(vels(target_linear_vel, target_angular_vel))
+            elif key == ' ' or key == 's':
+                target_linear_vel = 0.0
+                control_linear_vel = 0.0
+                target_angular_vel = 0.0
                 control_angular_vel = 0.0
                 print(vels(target_linear_vel, target_angular_vel))
             else:
-                if (key == '\x03'):
+                if key == '\x03':
                     break
 
             twist = Twist()
 
-            control_linear_vel = makeSimpleProfile(control_linear_vel, target_linear_vel, (LIN_VEL_STEP_SIZE/2.0))
-            twist.linear.x = control_linear_vel; twist.linear.y = 0.0; twist.linear.z = 0.0
+            control_linear_vel = makeSimpleProfile(control_linear_vel, target_linear_vel, (LIN_VEL_STEP_SIZE / 2.0))
+            twist.linear.x = control_linear_vel
+            twist.linear.y = 0.0
+            twist.linear.z = 0.0
 
-            control_angular_vel = makeSimpleProfile(control_angular_vel, target_angular_vel, (ANG_VEL_STEP_SIZE/2.0))
-            twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = control_angular_vel
+            control_angular_vel = makeSimpleProfile(control_angular_vel, target_angular_vel, (ANG_VEL_STEP_SIZE / 2.0))
+            twist.angular.x = 0.0
+            twist.angular.y = 0.0
+            twist.angular.z = control_angular_vel
 
             pub.publish(twist)
 
@@ -146,9 +148,16 @@ if __name__=="__main__":
 
     finally:
         twist = Twist()
-        twist.linear.x = 0.0; twist.linear.y = 0.0; twist.linear.z = 0.0
-        twist.angular.x = 0.0; twist.angular.y = 0.0; twist.angular.z = 0.0
+        twist.linear.x = 0.0
+        twist.linear.y = 0.0
+        twist.linear.z = 0.0
+        twist.angular.x = 0.0
+        twist.angular.y = 0.0
+        twist.angular.z = 0.0
         pub.publish(twist)
 
     if os.name != 'nt':
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+
+if __name__ == "__main__":
+    main()
